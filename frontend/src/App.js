dae10a18018811186658241c4a306f5d86a36f73
Pikian <sebastian@trollheimstudios.com>
2024-10-25 13:34:02 +0200

    fixing files and structures

---
+++ frontend/src/App.js
@@ -0,0 +1,173 @@
+import React, { useState } from 'react';
+
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+
+import TrackList from './components/TrackList';
+
+
+
+import AudioPlayer from 'react-h5-audio-player';
+
+
+
+import 'react-h5-audio-player/lib/styles.css';
+
+
+
+import './App.css';
+
+
+
+
+
+
+
+function App() {
+
+
+
+  const [currentTrack, setCurrentTrack] = useState(null);
+
+
+
+
+
+
+
+  const handlePlay = (track) => {
+
+
+
+    setCurrentTrack(track);
+
+
+
+  };
+
+
+
+
+
+
+
+  return (
+
+
+
+    <Router>
+
+
+
+      <div className="App">
+
+
+
+        <div className="content">
+
+
+
+          <Routes>
+
+
+
+            <Route path="/" element={<TrackList onPlayTrack={handlePlay} currentTrack={currentTrack} />} />
+
+
+
+          </Routes>
+
+
+
+        </div>
+
+
+
+        {currentTrack && (
+
+
+
+          <div className="player-container">
+
+
+
+            <div className="now-playing">
+
+
+
+              <h3>{currentTrack.name}</h3>
+
+
+
+            </div>
+
+
+
+            <AudioPlayer
+
+
+
+              src={`http://localhost:3001/tracks/${encodeURIComponent(currentTrack.filename)}`}
+
+
+
+              autoPlay
+
+
+
+              showSkipControls={false}
+
+
+
+              showJumpControls={true}
+
+
+
+              layout="stacked-reverse"
+
+
+
+            />
+
+
+
+          </div>
+
+
+
+        )}
+
+
+
+      </div>
+
+
+
+    </Router>
+
+
+
+  );
+
+
+
+}
+
+
+
+
+
+
+
+export default App;
+
+
+
+
+
+
+
+
